0c25be7465a14ef73cb8f56e39158ded55665790
Knight <knzphumthawan@gmail.com>
2021-07-11 11:27:21 +0700

    implement repo

--- repository/customer_db.go
+++ repository/customer_db.go
@@ -1,20 +1,33 @@
 package repository
 
-import "github.com/jmoiron/sqlx"
+import (
+	"github.com/jmoiron/sqlx"
+)
 
 type customerRepository struct {
 	db *sqlx.DB
 }
 
-func NewCustomerRepositoryDB(db *sqlx.DB) customerRepository {
+func NewCustomerRepository(db *sqlx.DB) customerRepository {
 	return customerRepository{db: db}
 }
 
 func (r customerRepository) GetAll() ([]Custumer, error) {
-
-	return nil, nil
+	customers := []Custumer{}
+	query := "select customer_id, name, date_of_birth, city, zipcode, status from customers"
+	err := r.db.Select(&customers, query)
+	if err != nil {
+		return nil, err
+	}
+	return customers, nil
 }
 
 func (r customerRepository) GetById(Id int) (*Custumer, error) {
-	return nil, nil
+	customer := Custumer{}
+	query := "select customer_id, name, date_of_birth, city, zipcode, status from customers where customer_id = ?"
+	err := r.db.Get(&customer, query, Id)
+	if err != nil {
+		return nil, err
+	}
+	return &customer, nil
 }
