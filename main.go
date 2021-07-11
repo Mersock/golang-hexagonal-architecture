f822a735ec271a971c3e3f4d1c66eeba3beeb3c2
Knight <knzphumthawan@gmail.com>
2021-07-11 14:25:00 +0700

    listen serv

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/Mersock/golang-hexagonal-architecture/handler"
 	"github.com/Mersock/golang-hexagonal-architecture/repository"
+	"github.com/Mersock/golang-hexagonal-architecture/service"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
 )
 
 func main() {
-	db, err := sqlx.Open("mysql", "banking:P@ssw0rd@tcp(db:3306)/banking")
+	db, err := sqlx.Open("mysql", "root:root@tcp(db:3306)/banking?parseTime=true")
 	if err != nil {
 		panic(err)
 	}
-	customerRepository := repository.NewCustomerRepositoryDB(db)
-	_ = customerRepository
+	router := mux.NewRouter()
+
+	customerRepository := repository.NewCustomerRepository(db)
+	customerService := service.NewCustomerService(customerRepository)
+	customerHandler := handler.NewCustomerHandler(customerService)
+
+	router.HandleFunc("/customers", customerHandler.GetCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers/{CustomerID:[0-9]+}", customerHandler.GetCustomer).Methods(http.MethodGet)
+
+	http.ListenAndServe(":8080", router)
 }
