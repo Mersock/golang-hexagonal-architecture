e9827a78f1b78cc73a1ae5102ebe37c14ed71519
Knight <knzphumthawan@gmail.com>
2021-07-11 14:24:43 +0700

    add customer handler

---
+++ handler/customer.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/Mersock/golang-hexagonal-architecture/service"
+	"github.com/gorilla/mux"
+)
+
+type customerHandler struct {
+	custService service.CustomerService
+}
+
+func NewCustomerHandler(custService service.CustomerService) customerHandler {
+	return customerHandler{
+		custService: custService,
+	}
+}
+
+func (h customerHandler) GetCustomers(w http.ResponseWriter, r *http.Request) {
+	customers, err := h.custService.GetCustomers()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, err)
+		return
+	}
+	w.Header().Set("content-type", "application/json")
+	json.NewEncoder(w).Encode(customers)
+}
+
+func (h customerHandler) GetCustomer(w http.ResponseWriter, r *http.Request) {
+	CustomerID, _ := strconv.Atoi(mux.Vars(r)["CustomerID"])
+	customer, err := h.custService.GetCustomer(CustomerID)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, err)
+		return
+	}
+	w.Header().Set("content-type", "application/json")
+	json.NewEncoder(w).Encode(customer)
+}
